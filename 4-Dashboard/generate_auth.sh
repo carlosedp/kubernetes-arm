421c611b5f95c3a54666a542850c34cfd5e0465b
CarlosEDP <me@carlosedp.com>
2018-05-03 16:29:45 -0500

    Update script

--- 4-Dashboard/generate_auth.sh
+++ 4-Dashboard/generate_auth.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [[ $# -eq 0 ]] ; then
-    echo "Run the script with the required auth user: ${0} [user]"
+    echo "Run the script with the required auth user and namespace for the secret: ${0} [user] [namespace]"
     exit 0
 fi
-printf "${1}:`openssl passwd -apr1`\n" >> auth
-kubectl delete secret -n kube-system dashboard-auth
-kubectl create secret generic dashboard-auth --from-file=auth -n kube-system
-rm auth
+printf "${1}:`openssl passwd -apr1`\n" >> ingress_auth.tmp
+kubectl delete secret -n ${2} ingress-auth
+kubectl create secret generic ingress-auth --from-file=auth -n ${2}
+rm ingress_auth.tmp
