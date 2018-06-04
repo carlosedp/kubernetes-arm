ce2461e5bd071e73118a5e1cd29796a074e70470
CarlosEDP <me@carlosedp.com>
2018-06-04 16:07:21 -0500

    Changed job to use an image that doesnt overwrite certificates. Add Consul ingress and Traefik resources for my cluster.

--- 2-Traefik/deploy_consul_cluster.sh
+++ 2-Traefik/deploy_consul_cluster.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 # Version 1.1.0 is needed because the consul manifest doesn't have the 1.0.0 image for ARM
 helm install --name traefik stable/consul --set ImageTag=1.1.0
+
+cat << EOF
+apiVersion: extensions/v1beta1
+kind: Ingress
+metadata:
+  name: consul-ui
+  namespace: kube-system
+spec:
+  rules:
+  - host: consul.internal.carlosedp.com
+    http:
+      paths:
+      - path: /
+        backend:
+          serviceName: consul-traefik-consul-ui
+          servicePort: http
+EOF | kubectl create -f -
